708f7d4e48236321c881d00f10d7e1f24ce70cc1
Marcel Jerzyk <244979@student.pwr.edu.pl>
2022-05-02 11:43:54 +0200

    [`0.3.2+7`] C: Updated Dependencies Versions

--- src/main.py
+++ src/main.py
@@ -4,7 +4,7 @@ from gitversioniser.domain.versioniser.versioniser import Versioniser
 from gitversioniser.helpers.logger import CONSOLE
 
 
-__version__ = '0.3.2+build.6'
+__version__ = '0.3.2+build.7'
 
 
 def config() -> Config:
